8fcc18269a00302f315e61750ee13fe1df9faf5e
antiluddite <88464949+antiluddite@users.noreply.github.com>
2023-07-29 15:51:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function magic8ball() {
+    var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+    var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+    return randomAnswer;
+  }
+  console.log(magic8ball)
---
+++ data.js
@@ -0,0 +1,47 @@
+export const CHARACTERS = [
+    {
+      id: 1,
+      name: 'Gordon',
+      cunning: 29,
+      stealth: 5,
+      brutality: 81,
+      skills:16,
+      charm: 1
+    },
+    {
+        id: 2,
+        name: 'Gilbert',
+        cunning: 9,
+        stealth: 78,
+        brutality: 87,
+        skills:66,
+        charm: 56
+      },
+      {
+        id: 3,
+        name: 'Basil',
+        cunning: 41,
+        stealth: 16,
+        brutality: 62,
+        skills:1,
+        charm: 5
+      },
+      {
+        id: 4,
+        name: 'Ruth',
+        cunning: 99,
+        stealth: 10,
+        brutality: 25,
+        skills:76,
+        charm: 1
+      },
+      {
+        id: 5,
+        name: 'Mildred',
+        cunning: 67,
+        stealth: 33,
+        brutality: 17,
+        skills:2,
+        charm: 48
+      },
+]
